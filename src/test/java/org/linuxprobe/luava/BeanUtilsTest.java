85859ea80a49aa011502328e6ecae17bfbd1de47
larryyu <linuxprobe.org@gmail.com>
2019-10-14 14:08:19 +0800

    update

--- src/test/java/org/linuxprobe/luava/BeanUtilsTest.java
+++ src/test/java/org/linuxprobe/luava/BeanUtilsTest.java
@@ -1,36 +1,36 @@
 package org.linuxprobe.luava;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.junit.Test;
 import org.linuxprobe.luava.reflection.BeanUtils;
 import org.linuxprobe.luava.reflection.BeanUtils.CopyOptions;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class BeanUtilsTest {
-	@Test
-	public void test() {
-		try {
-			StudentA studentA = new StudentA();
-			StudentB studentB = new StudentB();
-			BeanUtils.copyProperties(studentA, studentB, new CopyOptions().addFieldMapping("height", "heIght"));
-			System.out.println(studentB);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @Test
+    public void test() {
+        try {
+            StudentA studentA = new StudentA();
+            StudentB studentB = new StudentB();
+            BeanUtils.copyProperties(studentA, studentB, new CopyOptions().addFieldMapping("height", "heIght"));
+            System.out.println(studentB);
+            System.out.println(BeanUtils.beanToMap(studentA));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 
 @Getter
 @Setter
 class StudentA {
-	static String test = "testa";
-	private Integer height = 78;
+    static String test = "testa";
+    private Integer height = 78;
 }
 
 @Getter
 @Setter
 class StudentB {
-	static String test = "test";
-	private int heIght = 165;
+    static String test = "test";
+    private int heIght = 165;
 }
